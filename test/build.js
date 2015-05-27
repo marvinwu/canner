f4e90e70ab3cb8da7a55c3383974b9ce45c03043
william <wwwy3y3@gmail.com>
2015-05-27 19:48:52 +0800

    jsdom module in md-attr is too old, if we upgrade it, the space is not we expected before, so we have to delete all spaces

--- test/build.js
+++ test/build.js
@@ -11,12 +11,13 @@ rm_html = exec('rm ./test/**/**/index.html', function(err) {
 		console.error(err)
 })
 
+
 describe('build in handlebars canner.json', function() {
 	it("should build canner.json to html", function(done) {
 		canner.build(__dirname + '/hbs/original/canner.json', {output: __dirname + '/hbs/original'})
 			.then(function() {
-				var output = fs.readFileSync(__dirname + '/hbs/original/index.html', {encoding: 'utf8'});
-				var result = fs.readFileSync(__dirname + '/result/hbs_original.html', {encoding: 'utf8'});
+				var output = fs.readFileSync(__dirname + '/hbs/original/index.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+				var result = fs.readFileSync(__dirname + '/result/hbs_original.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
 				assert.equal(output, result);
 				done();
@@ -29,8 +30,8 @@ describe('build in handlebars canner.json', function() {
 	it("should build canner.json to html with hbs helper", function(done) {
 		canner.build(__dirname + '/hbs/helper/canner.json', {output: __dirname + '/hbs/helper'})
 			.then(function() {
-				var output = fs.readFileSync(__dirname + '/hbs/helper/index.html', {encoding: 'utf8'});
-				var result = fs.readFileSync(__dirname + '/result/hbs_helper.html', {encoding: 'utf8'});
+				var output = fs.readFileSync(__dirname + '/hbs/helper/index.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+				var result = fs.readFileSync(__dirname + '/result/hbs_helper.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
 				assert.equal(output, result);
 				done();
@@ -43,8 +44,8 @@ describe('build in handlebars canner.json', function() {
 	it("should build canner.yaml to html", function(done) {
 		canner.build(__dirname + '/hbs/yaml/canner.yaml', {output: __dirname + '/hbs/yaml'})
 			.then(function() {
-				var output = fs.readFileSync(__dirname + '/hbs/yaml/index.html', {encoding: 'utf8'});
-				var result = fs.readFileSync(__dirname + '/result/hbs_yaml.html', {encoding: 'utf8'});
+				var output = fs.readFileSync(__dirname + '/hbs/yaml/index.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+				var result = fs.readFileSync(__dirname + '/result/hbs_yaml.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
 				assert.equal(output, result);
 				done();
@@ -59,8 +60,8 @@ describe('build in nunjucks canner.json', function() {
 	it("should build canner.json to html", function(done) {
 		canner.build(__dirname + '/nunjucks/original/canner.json', {output: __dirname + '/nunjucks/original', engine: 'nunjucks'})
 			.then(function() {
-				var output = fs.readFileSync(__dirname + '/nunjucks/original/index.html', {encoding: 'utf8'});
-				var result = fs.readFileSync(__dirname + '/result/nun_original.html', {encoding: 'utf8'});
+				var output = fs.readFileSync(__dirname + '/nunjucks/original/index.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+				var result = fs.readFileSync(__dirname + '/result/nun_original.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
 				assert.equal(output, result);
 				done();
@@ -74,8 +75,8 @@ describe('build in nunjucks canner.json', function() {
 	it("should build canner.yaml to html", function(done) {
 		canner.build(__dirname + '/nunjucks/yaml/canner.yaml', {output: __dirname + '/nunjucks/yaml', engine: 'nunjucks'})
 			.then(function() {
-				var output = fs.readFileSync(__dirname + '/nunjucks/yaml/index.html', {encoding: 'utf8'});
-				var result = fs.readFileSync(__dirname + '/result/nun_yaml.html', {encoding: 'utf8'});
+				var output = fs.readFileSync(__dirname + '/nunjucks/yaml/index.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+				var result = fs.readFileSync(__dirname + '/result/nun_yaml.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
 				assert.equal(output, result);
 				done();
@@ -90,8 +91,8 @@ describe('build in jade canner.json', function() {
 	it("should build canner.json to html", function(done) {
 		canner.build(__dirname + '/jade/original/canner.json', {output: __dirname + '/jade/original', engine: 'jade'})
 			.then(function() {
-				var output = fs.readFileSync(__dirname + '/jade/original/index.html', {encoding: 'utf8'});
-				var result = fs.readFileSync(__dirname + '/result/jade_original.html', {encoding: 'utf8'});
+				var output = fs.readFileSync(__dirname + '/jade/original/index.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+				var result = fs.readFileSync(__dirname + '/result/jade_original.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
 				assert.equal(output, result);
 				done();
@@ -105,8 +106,8 @@ describe('build in jade canner.json', function() {
 	it("should build canner.yaml to html", function(done) {
 		canner.build(__dirname + '/jade/yaml/canner.yaml', {output: __dirname + '/jade/yaml', engine: 'jade'})
 			.then(function() {
-				var output = fs.readFileSync(__dirname + '/jade/yaml/index.html', {encoding: 'utf8'});
-				var result = fs.readFileSync(__dirname + '/result/jade_yaml.html', {encoding: 'utf8'});
+				var output = fs.readFileSync(__dirname + '/jade/yaml/index.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+				var result = fs.readFileSync(__dirname + '/result/jade_yaml.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
 				assert.equal(output, result);
 				done();
@@ -121,8 +122,8 @@ describe('build in swig canner.json', function() {
 	it("should build canner.json to html", function(done) {
 		canner.build(__dirname + '/swig/original/canner.json', {output: __dirname + '/swig/original', engine: 'swig'})
 			.then(function() {
-				var output = fs.readFileSync(__dirname + '/swig/original/index.html', {encoding: 'utf8'});
-				var result = fs.readFileSync(__dirname + '/result/swig_original.html', {encoding: 'utf8'});
+				var output = fs.readFileSync(__dirname + '/swig/original/index.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+				var result = fs.readFileSync(__dirname + '/result/swig_original.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
 				assert.equal(output, result);
 				done();
@@ -136,8 +137,8 @@ describe('build in swig canner.json', function() {
 	it("should build canner.yaml to html", function(done) {
 		canner.build(__dirname + '/swig/yaml/canner.yaml', {output: __dirname + '/swig/yaml', engine: 'swig'})
 			.then(function() {
-				var output = fs.readFileSync(__dirname + '/swig/yaml/index.html', {encoding: 'utf8'});
-				var result = fs.readFileSync(__dirname + '/result/swig_yaml.html', {encoding: 'utf8'});
+				var output = fs.readFileSync(__dirname + '/swig/yaml/index.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+				var result = fs.readFileSync(__dirname + '/result/swig_yaml.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
 				assert.equal(output, result);
 				done();
@@ -152,8 +153,8 @@ describe('build in mustache canner.json', function() {
 	it("should build canner.json to html", function(done) {
 		canner.build(__dirname + '/mustache/original/canner.json', {output: __dirname + '/mustache/original', engine: 'mustache'})
 			.then(function() {
-				var output = fs.readFileSync(__dirname + '/mustache/original/index.html', {encoding: 'utf8'});
-				var result = fs.readFileSync(__dirname + '/result/mus.html', {encoding: 'utf8'});
+				var output = fs.readFileSync(__dirname + '/mustache/original/index.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+				var result = fs.readFileSync(__dirname + '/result/mus.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
 				assert.equal(output, result);
 				done();
@@ -167,8 +168,8 @@ describe('build in mustache canner.json', function() {
 	it("should build canner.yaml to html", function(done) {
 		canner.build(__dirname + '/mustache/yaml/canner.yaml', {output: __dirname + '/mustache/yaml', engine: 'mustache'})
 			.then(function() {
-				var output = fs.readFileSync(__dirname + '/mustache/yaml/index.html', {encoding: 'utf8'});
-				var result = fs.readFileSync(__dirname + '/result/mus.html', {encoding: 'utf8'});
+				var output = fs.readFileSync(__dirname + '/mustache/yaml/index.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+				var result = fs.readFileSync(__dirname + '/result/mus.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
 				assert.equal(output, result);
 				done();
@@ -183,8 +184,8 @@ describe('build in dust canner.json', function() {
 	it("should build canner.json to html", function(done) {
 		canner.build(__dirname + '/dust/original/canner.json', {output: __dirname + '/dust/original', engine: 'dust'})
 			.then(function() {
-				var output = fs.readFileSync(__dirname + '/dust/original/index.html', {encoding: 'utf8'});
-				var result = fs.readFileSync(__dirname + '/result/dust.html', {encoding: 'utf8'});
+				var output = fs.readFileSync(__dirname + '/dust/original/index.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+				var result = fs.readFileSync(__dirname + '/result/dust.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
 				assert.equal(output, result);
 				done();
@@ -198,8 +199,8 @@ describe('build in dust canner.json', function() {
 	it("should build canner.yaml to html", function(done) {
 		canner.build(__dirname + '/dust/yaml/canner.yaml', {output: __dirname + '/dust/yaml', engine: 'dust'})
 			.then(function() {
-				var output = fs.readFileSync(__dirname + '/dust/yaml/index.html', {encoding: 'utf8'});
-				var result = fs.readFileSync(__dirname + '/result/dust.html', {encoding: 'utf8'});
+				var output = fs.readFileSync(__dirname + '/dust/yaml/index.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+				var result = fs.readFileSync(__dirname + '/result/dust.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
 				assert.equal(output, result);
 				done();
@@ -222,8 +223,8 @@ describe('build using object', function () {
 				}
 			})
 		.done(function () {
-			var result = fs.readFileSync(__dirname + '/result/hbs_data_input.html', {encoding: 'utf8'});
-			var html= fs.readFileSync(path.resolve(output, './index.html'), 'utf8');
+			var result = fs.readFileSync(__dirname + '/result/hbs_data_input.html', {encoding: 'utf8'}).replace(/\s+/g, '');
+			var html= fs.readFileSync(path.resolve(output, './index.html'), 'utf8').replace(/\s+/g, '');
 			assert.equal(html, result);
 			done();
 		}, function (err) {
@@ -244,9 +245,9 @@ describe('build using object', function () {
 				returnContent: true
 			})
 		.done(function (html) {
-			var result = fs.readFileSync(__dirname + '/result/hbs_data_input.html', {encoding: 'utf8'});
+			var result = fs.readFileSync(__dirname + '/result/hbs_data_input.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
-			assert.equal(html, result);
+			assert.equal(html[0].replace(/\s+/g, ''), result);
 			done();
 		}, function (err) {
 			console.log(err);
@@ -266,9 +267,9 @@ describe('build using object', function () {
 				returnContent: true
 			})
 		.done(function (html) {
-			var result = fs.readFileSync(__dirname + '/result/hbs_data_input.html', {encoding: 'utf8'});
+			var result = fs.readFileSync(__dirname + '/result/hbs_data_input.html', {encoding: 'utf8'}).replace(/\s+/g, '');
 
-			assert.equal(html, result);
+			assert.equal(html[0].replace(/\s+/g, ''), result);
 			done();
 		}, function (err) {
 			console.log(err);
