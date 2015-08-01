9b73b1f9114bc1b2e55dc19c54372061895e3591
william <wwwy3y3@gmail.com>
2015-08-01 22:12:17 +0800

    fix api in index.js, all api from core should still work in canner

--- index.js
+++ index.js
@@ -5,80 +5,11 @@ var allin_html = require('allin');
 var path = require('path');
 var Q = require('q');
 
-var build = require('./lib/build');
-var init = require('./lib/init');
-var get = require('./lib/get');
-var read = require('./lib/read');
+var core= require('canner-core');
 
-/*
- *  Init
- *  Initialize files and folders, under a directory.
- *  @param {string} dir - directory install canner
- *  @param {string} generator - Inital generate the generator that you are finding
- */
-exports.init = function(dir, generator) {
-  return init(dir, generator);
-}
-
-/*
- *  Build
- *  Build a canner from a canner.json
- *  @param {string} dir - source to canner.json, default ./canner.json
- *  @param {object} options - options
- */
-exports.build = function(dir, options) {
-  return build.folder(dir, options, false)
-}
-
-/*
- *  Watch
- *  Watching any changes in a canner and recompiled
- *  @param {string} dir - source to canner.json, default ./canner.json
- *  @param {object} options - options
- */
-exports.watch = function(dir, options) {
-  return build.folder(dir, options, true)
-}
-
-/*
- *  Allin
- *  Make html include files all warp allin
- *  @param {string} htmlfile - source to your html, default ./index.html
- *  @param {object} options - options
- */
-
-exports.allin = function(htmlfile, options) {
-  var options = {
-    "minifyCSS": true,
-    "minifyJS": true,
-    "removeComments": true,
-    "collapseWhitespace": true
-  };
-
-  var filename = options.filename || 'output.html';
-  var output = options.output || './';
-  var minify = options.minifyall || null;
-
-  if (!minify) {
-    options = null;
-  }
-
-  allin_html(
-    path.resolve(process.cwd(), htmlfile),
-    options,
-    function(all) {
-      fileSave(path.resolve(path.join(process.cwd(), output, filename)))
-        .write(all);
-    });
-}
-
-/*
- * read
- * read a file path
- * @param {string} can - can name, e.g., sample-can
- * @param {string} filePath - file path you want to read
- */
-
-exports.read = function(can, filePath) {
-  return read(can, filePath);
-}
+// core api
+exports.init = core.init;
+exports.build = core.build;
+exports.watch = core.watch;
+exports.allin = core.allin;
+exports.read = core.read;
