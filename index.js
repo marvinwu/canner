18d06b7a7f37c6c895272466c96cfd79d5b9eb18
chihsuan <chihsuan.huang.7@gmail.com>
2015-08-01 18:03:26 +0800

    update indent

--- index.js
+++ index.js
@@ -8,77 +8,77 @@ var Q = require('q');
 var build = require('./lib/build');
 var init = require('./lib/init');
 var get = require('./lib/get');
-var read= require('./lib/read');
+var read = require('./lib/read');
 
 /*
-*	Init
-*	Initialize files and folders, under a directory.
-*	@param {string} dir - directory install canner
-*	@param {string} generator - Inital generate the generator that you are finding
-*/
-exports.init= function (dir, generator) {
-    return init(dir, generator);
+ *  Init
+ *  Initialize files and folders, under a directory.
+ *  @param {string} dir - directory install canner
+ *  @param {string} generator - Inital generate the generator that you are finding
+ */
+exports.init = function(dir, generator) {
+  return init(dir, generator);
 }
 
 /*
-*	Build
-*	Build a canner from a canner.json
-*	@param {string} dir - source to canner.json, default ./canner.json
-*	@param {object} options - options
-*/
-exports.build= function (dir, options) {
-	return build.folder(dir, options, false)
+ *  Build
+ *  Build a canner from a canner.json
+ *  @param {string} dir - source to canner.json, default ./canner.json
+ *  @param {object} options - options
+ */
+exports.build = function(dir, options) {
+  return build.folder(dir, options, false)
 }
 
 /*
-*	Watch
-*	Watching any changes in a canner and recompiled
-*	@param {string} dir - source to canner.json, default ./canner.json
-*	@param {object} options - options
-*/
-exports.watch= function (dir, options) {
-	return build.folder(dir, options, true)
+ *  Watch
+ *  Watching any changes in a canner and recompiled
+ *  @param {string} dir - source to canner.json, default ./canner.json
+ *  @param {object} options - options
+ */
+exports.watch = function(dir, options) {
+  return build.folder(dir, options, true)
 }
 
 /*
-*	Allin
-*	Make html include files all warp allin
-*	@param {string} htmlfile - source to your html, default ./index.html
-*	@param {object} options - options
-*/
+ *  Allin
+ *  Make html include files all warp allin
+ *  @param {string} htmlfile - source to your html, default ./index.html
+ *  @param {object} options - options
+ */
 
-exports.allin= function (htmlfile, options) {
-	var options = {
-      "minifyCSS": true,
-      "minifyJS": true,
-      "removeComments":true,
-      "collapseWhitespace": true
-    };
+exports.allin = function(htmlfile, options) {
+  var options = {
+    "minifyCSS": true,
+    "minifyJS": true,
+    "removeComments": true,
+    "collapseWhitespace": true
+  };
 
-    var filename = options.filename || 'output.html';
-    var output = options.output || './';
-    var minify = options.minifyall || null;
+  var filename = options.filename || 'output.html';
+  var output = options.output || './';
+  var minify = options.minifyall || null;
 
-    if(!minify) {
-      options = null;
-    }
+  if (!minify) {
+    options = null;
+  }
 
-    allin_html(
-    	path.resolve(process.cwd(), htmlfile),
-    	options,
-    	function(all) {
-      		fileSave(path.resolve(path.join(process.cwd(), output, filename)))
-        	.write(all);
-    	});
+  allin_html(
+    path.resolve(process.cwd(), htmlfile),
+    options,
+    function(all) {
+      fileSave(path.resolve(path.join(process.cwd(), output, filename)))
+        .write(all);
+    });
 }
 
 /*
-* read
-* read a file path
-* @param {string} can - can name, e.g., sample-can
-* @param {string} filePath - file path you want to read
-*/
+ * read
+ * read a file path
+ * @param {string} can - can name, e.g., sample-can
+ * @param {string} filePath - file path you want to read
+ */
 
-exports.read= function (can, filePath) {
+exports.read = function(can, filePath) {
   return read(can, filePath);
 }
