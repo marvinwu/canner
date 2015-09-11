eb64fb5e1dac4255d59ccc587940940249a973c8
chihsuan <chihsuan.huang.7@gmail.com>
2015-09-11 22:54:53 +0800

    add data:new

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 var core= require('canner-core');
+var apps = require('./lib/apps');
+var auth = require('./lib/auth');
+var data = require('./lib/data');
+var git = require('./lib/git');
+var gh = require('./lib/gh');
 
 // core api
 exports.init = core.init;
@@ -6,3 +11,8 @@ exports.build = core.build;
 exports.watch = core.watch;
 exports.allin = core.allin;
 exports.read = core.read;
+exports.auth = auth;
+exports.apps = apps;
+exports.data = data;
+exports.git = git;
+exports.gh = gh;
